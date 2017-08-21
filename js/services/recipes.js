4e345327118e78aed3d170c6b9709b09ff7b70da
Perry Fustero <pjfust@gmail.com>
2017-08-21 12:30:48 -0400

    bug fixes, local host to routes

--- js/services/recipes.js
+++ js/services/recipes.js
@@ -1,13 +1,13 @@
 /* global angular */
 angular.module('myrecipe')
-.factory('RecipeFactory',[
+.factory('RecipesFactory',[
   '$resource',
   RecipeService
 ])
 
 
 function RecipeService($resource){
-  return $resource('https://api.edamam.com/search?q=salmon&app_id=495091ab&app_key=53498ba8d4b2061499db5197b986469e', {}, {
+  return $resource('http://localhost:3000/recipes/:id.json', {}, {
     update: {
       method: 'PUT'
     }
