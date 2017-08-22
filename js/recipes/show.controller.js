5628f956b02f696a0f533341e8355a0703b27514
Perry Fustero <pjfust@gmail.com>
2017-08-22 18:50:28 -0400

    ingredients available on show path

--- js/recipes/show.controller.js
+++ js/recipes/show.controller.js
@@ -11,4 +11,7 @@ angular
 
 function RecipesShowControllerFunction($stateParams, RecipesFactory){
   this.recipe = RecipesFactory.get({id: $stateParams.id})
+  this.recipe.$promise.then((r) => {
+    return r.name
+  }).then(name => {console.log(name)})
 }
