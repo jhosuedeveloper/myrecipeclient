b680fc6cb008257f6790eb49409d55af0aaa5276
Perry Fustero <pjfust@gmail.com>
2017-08-22 09:42:56 -0400

    return button now searches

--- js/recipes/index.controller.js
+++ js/recipes/index.controller.js
@@ -13,13 +13,24 @@ function RecipesIndexControllerFunction(RecipesFactory){
     search: queries
   })
 
-  this.search = function (q) {
-    console.log(q)
-    queries.push(q)
+  this.search = function (search) {
+    console.log(search)
+    queries.push(search)
     console.log(queries)
     // $state.go("recipesIndex", {}, {reload:"recipesIndex"})
     this.recipes = RecipesFactory.query({
       search: queries
     })
+  }
+
+  this.enterSearch = function (search, $event) {
+    if ($event.keyCode === 13) {
+      console.log($event.keyCode)
+      queries.push(search)
+      this.recipes = RecipesFactory.query({
+        search: queries
+      })
+      $event.preventDefault()
+    }
   }
 }
